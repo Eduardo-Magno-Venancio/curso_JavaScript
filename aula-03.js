d61b3e568b01ea547fe03b4fc42c1a8fae37bd4b
Eduardo Magno <solidmg09@gmail.com>
2024-11-16 12:05:13 -0300

    feat: aula03

---
+++ aula-03.js
@@ -0,0 +1,13 @@
+const idade = 18;
+const nome = 'edu';
+
+
+if (idade ==='18' ){
+
+    console.log('ENTROU!!!');
+
+}else {
+    console.log("NÃO PODE ENTGRAR!!!")
+}
+
+//console.log ('Usuario pode entrar', podeEntrar)
