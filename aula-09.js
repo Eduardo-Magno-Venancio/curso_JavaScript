d559e291099caf8b2fa6ddf12fc2ee395fdddd93
Eduardo Magno <solidmg09@gmail.com>
2024-11-23 17:50:49 -0300

    feat: aula-09

---
+++ aula-09.js
@@ -0,0 +1,13 @@
+ 
+function soma(item1, item2){
+  return item1 +item2;
+}
+
+function diminuir(item1,item2){
+  return item1 - item2
+
+}
+
+console.log('SOMA', soma(5,6))
+console.log('SOMA', soma(5,642423543))
+console.log('DIMINUIR', diminuir(5,642423543))
