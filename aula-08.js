a037025e031fcfc421a7732ebc09d44986dbb1b9
Eduardo Magno <solidmg09@gmail.com>
2024-11-23 17:29:06 -0300

    feat: aula-08

---
+++ aula-08.js
@@ -0,0 +1,5 @@
+const salario = "103";
+const salarioNumero = Number(salario);
+console.log('salario:', salario)
+console.log('slario numero:', salarioNumero)
+console.log('slario numero:', salarioNumero.toFixed(2))
